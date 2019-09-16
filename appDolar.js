a2f6381e70ec5088383a76cc028f49767f962f89
Federico Aguero <federicomatias.aguero1987@gmail.com>
2019-09-16 17:56:10 -0300

    se agrego cotizador

--- appDolar.js
+++ appDolar.js
@@ -1,7 +1,28 @@
 const dolar= require('./dolar/cotizacion');
 
-dolar.getCotizacionDolar().then(console.log);
+const argv = require('yargs').options({
+    monedaOrigen:{
+        alias:'o',
+        des:'moneda a cotizar',
+        demmand:true
+    },
+    monedaDestino:{
+        alias:'d',
+        des:'moneda a cotizada',
+        demmand:true 
+    },
+    importe:{
+        alias:'i',
+        des:'importe a invertir',
+        demmand:true
+    }
 
+}).argv;
+
+//dolar.getCotizacionDolar().then(console.log);
+dolar.getCotizacionInversion(argv.monedaOrigen,argv.monedaDestino,argv.importe)
+     .then(console.log);
+console.log(`${argv.monedaOrigen} | ${argv.monedaDestino} | ${argv.importe}`);
 
 
 
--- dolar/cotizacion.js
+++ dolar/cotizacion.js
@@ -5,24 +5,48 @@ getCotizacionDolar=async ()=>{
     const token="eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTk5MTAxMzYsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmZWRlcmljb21hdGlhcy5jZWxlc3RlQGdtYWlsLmNvbSJ9.D6JzwbWmobZW-eG7nwWx0ctLXkNIJUNKF3RJCeHhaVGDc0y5MRfzZM8R8eSjgt54vuxB5Q1bk6gfFUv_LhW1-A";
     
     const instance = axios.create({
-        baseURL: `https://api.estadisticasbcra.com/usd`,
-        headers: {"Authorization" : 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTk5Mzk3OTQsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmZWRlcmljb21hdGlhcy5jZWxlc3RlQGdtYWlsLmNvbSJ9.T20biJ7vrtXtCHiZTQEbZKFW9589Z1iNwdBgetnWCl7afpt4pUlbtmzQABi2HjxPPw6qrk8IUPTJDT1FFIW4ZQ'}
-//           body: {'token':'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTk5MTAxMzYsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmZWRlcmljb21hdGlhcy5jZWxlc3RlQGdtYWlsLmNvbSJ9.D6JzwbWmobZW-eG7nwWx0ctLXkNIJUNKF3RJCeHhaVGDc0y5MRfzZM8R8eSjgt54vuxB5Q1bk6gfFUv_LhW1-A'}
+        baseURL: `https://api.estadisticasbcra.com/usd_of_minorista`,
+        headers: {"Authorization" : 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDAxOTQ5MjksInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmZWRlcmljb21hdGlhcy5hZ3Vlcm8xOTg3QGdtYWlsLmNvbSJ9.jU_zdqxAJcRc3nMdT275p-406iqLd59ZIkgY7ODLtOLMomeg6ksHTqkyFAhKCQzsnL9PkyX5yLZ9IFPwlrb60A'}
     })
     
-    //const resp= await instance.get();
-    const resp = json[ Object.keys(await instance.get()).sort().pop() ];
-    
-    
+    const resp = await instance.get();
+
     if(resp.data.Results===""){
         throw new Error(`No hay resultados para el dolar`)
     }
    
-    return resp;
+    return resp.data[resp.data.length-1];
 }
 
+
+getCotizacionInversion=async (source,target,quantity)=>{
+                  
+    const monOri=encodeURI(source);
+    const monDes=encodeURI(target);
+    const imp=encodeURI(quantity);
+    
+    
+    const YOUR_KEY="2371|QK36CDDsFJWXxHUzdGvD3Pp0r4AXFr3H";
+    
+    const instance = axios.create({
+         
+        baseURL: `http://api.currencies.zone/v1/quotes/${monOri}/${monDes}/json?quantity=${imp}&key=${YOUR_KEY}`})
+    //`http://api.currencies.zone/v1/quotes/${monedaOrigen}/${monedaDestino}/json?quantity=${importe}&key=${YOUR_KEY}`})
+    
+    const resp = await instance.get();
+
+    if(resp.data.Results===""){
+        throw new Error(`No hay resultados para el dolar`)
+    }
+   
+    return resp.data;
+}
+
+
+
 module.exports={
-    getCotizacionDolar
+    getCotizacionDolar,
+    getCotizacionInversion
 }
 
 
